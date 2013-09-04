0e9fe1b4118850d08f7d1d8ea7d3e388a1f44da4
Aja Huang <ajahuang@gmail.com>
2013-09-04 10:05:09 +0100

    Improve 10912 - Simple Minded Hashing.cpp.

--- 10912 - Simple Minded Hashing.cpp
+++ 10912 - Simple Minded Hashing.cpp
@@ -2,28 +2,30 @@
 #include <cstring>
 using namespace std;
 
-static const int MAX_LENGTH = ((1 + 26) * 26) / 2;
-int memo[27][27][MAX_LENGTH + 1];
+static const int MAX_VALUE = ((1 + 26) * 26) / 2;
+int memo[27][27][MAX_VALUE + 1];
 
 int main()
 {   
     memset(memo, 0, sizeof memo);
-    memo[0][0][0] = 1;
-
+    for (int i = 1; i <= 26; ++i)
+        memo[i][1][i] = 1;
     /**
         memo[i][l][s] stores the number of strings of length l 
         and value s, considering up to letter i. So,
             memo[i][l][s] = memo[i - 1][l][s] 
                           + memo[i - 1][l - 1][s - i]
+
+        The base cases are memo[i][1][i] = 1 for i = 1 to 26. 
     */
-    for (int i = 1; i <= 26; ++i)
-        for (int l = 0; l <= i; ++l)
-            for (int s = 0; s <= MAX_LENGTH; ++s)
+    for (int i = 2; i <= 26; ++i)
+        for (int l = 1; l <= i; ++l)
+            for (int s = 1; s <= MAX_VALUE; ++s)
             {
                 // Letter i is not selected.
-                memo[i][l][s] = memo[i - 1][l][s];
+                memo[i][l][s] += memo[i - 1][l][s];
                 // Letter i is selected.
-                if (l > 0 && s >= i)
+                if (l >= 1 && s >= i)
                     memo[i][l][s] += memo[i - 1][l - 1][s - i];
             }
 
@@ -33,7 +35,7 @@ int main()
          cout << "Case " 
               << T++ 
               << ": ";
-         if (L > 26 || S > MAX_LENGTH)
+         if (L > 26 || S > MAX_VALUE)
              cout << "0" << endl;
          else
              cout << memo[26][L][S] << endl;
