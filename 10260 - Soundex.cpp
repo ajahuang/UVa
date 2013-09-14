4402d28791a56b041e29f0b1aefab66db1aa366a
Aja Huang <ajahuang@gmail.com>
2013-09-14 10:34:25 +0100

    Improve the solution of 10260 - Soundex.

--- 10260 - Soundex.cpp
+++ 10260 - Soundex.cpp
@@ -5,42 +5,34 @@ using namespace std;
 int main()
 {  
     // Prepare the mapping of Soundex coding.
-    char digits[26] = {0};
-    digits['B'-'A'] = digits['F'-'A'] 
-                    = digits['P'-'A']
-                    = digits['V'-'A']
-                    = '1';
-    digits['C'-'A'] = digits['G'-'A']
-                    = digits['J'-'A']
-                    = digits['K'-'A']
-                    = digits['Q'-'A']
-                    = digits['S'-'A']
-                    = digits['X'-'A']
-                    = digits['Z'-'A']
-                    = '2';
-    digits['D'-'A'] = digits['T'-'A'] 
-                    = '3';
-    digits['L'-'A'] = '4';
-    digits['M'-'A'] = digits['N'-'A'] 
-                    = '5';
-    digits['R'-'A'] = '6';
+    string soundex[6] = { "BFPV", 
+                          "CGJKQSXZ",
+                          "DT",
+                          "L",
+                          "MN",
+                          "R" };
+    size_t soundexSize = sizeof(soundex) / sizeof(soundex[0]);
 
     string word;
     while (cin >> word)
     {
         string code;
-        char lastLetter = ' ';
+        size_t prevIndex = soundexSize;
         for (size_t i = 0; i < word.size(); ++i)
         {
-            size_t target = word[i] - 'A';
-            // "repeated letters with the same code digit are 
-            // represented by a single instance of that digit."
-            if (digits[target] != 0
-                && digits[target] != digits[lastLetter - 'A'])
+            size_t j = 0;
+            for ( ; j < soundexSize; ++j)
             {
-                code.push_back(digits[target]);
+                if (soundex[j].find(word[i]) != string::npos)
+                {
+                    if (j != prevIndex)
+                        code.push_back(static_cast<char>('0' + j + 1));
+                    prevIndex = j;
+                    break;
+                }
             }
-            lastLetter = word[i];
+            if (j == soundexSize)
+                prevIndex = soundexSize;
         }
         cout << code << endl;
     }
