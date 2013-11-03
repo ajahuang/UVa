a0daf9f4c67447114116c68b338d8ca1cb18ed41
Aja Huang <ajahuang@gmail.com>
2013-11-03 20:22:36 +0000

    A minor change.

--- UVa 567 - Risk.cpp
+++ UVa 567 - Risk.cpp
@@ -31,7 +31,7 @@ int main()
                 break;
         } while (cin >> n);
 
-        // Floyd¡VWarshall algorithm.
+        // Floyd-Warshall algorithm.
         for (int k = 1; k <= N; ++k)
             for (int i = 1; i <= N; ++i)
                 for (int j = 1; j <= N; ++j)
